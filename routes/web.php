751a024e29ee0a5309a43f7c822231c9cee33802
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-09-28 14:30:29 +0700

    add index route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\PostController;
 //route resource
 Route::resource('/', PostController::class);
 
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
